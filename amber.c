e1859c0cee53e05e69ba18aab32b6a4831d8c390
Craig Peacock <craigp@beyondlogic.org>
2020-12-09 04:21:25 -0800

    Added start of parser

--- amber.c
+++ amber.c
@@ -5,8 +5,10 @@
 #include <curl/curl.h>
 #include <cjson/cJSON.h>
 
+void parse_amber_json(char *ptr);
+
 struct buffer {
-	const char *data;
+	char *data;
 	size_t pos;
 };
 
@@ -14,7 +16,7 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *user_data)
 {
 	struct buffer *out_buf = (struct buffer *)user_data;
 
-	printf("size %ld, nmemb %ld\r\n",size, nmemb);
+	//printf("size %ld, nmemb %ld\r\n",size, nmemb);
 
 	memcpy((void *)(out_buf->data + out_buf->pos), ptr, size * nmemb);
 	out_buf->pos += size * nmemb;
@@ -76,8 +78,9 @@ int main(void)
 		if(res != CURLE_OK) {
 			fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 		} else {
-			printf("Bytes read %ld\r\n",out_buf.pos);
-			printf("[%.*s]\r\n",(int)out_buf.pos, out_buf.data);
+			//printf("Bytes read %ld\r\n",out_buf.pos);
+			//printf("[%.*s]\r\n",(int)out_buf.pos, out_buf.data);
+			parse_amber_json(out_buf.data);
 		}
 
 		/* always cleanup */
@@ -87,4 +90,46 @@ int main(void)
 	curl_global_cleanup();
 
 	return 0;
+}
+
+void parse_amber_json(char *ptr)
+{
+	double price_kwh;
+
+	const cJSON *data;
+	const cJSON *parameter;
+
+	cJSON *NEM = cJSON_Parse(ptr);
+
+	data = cJSON_GetObjectItemCaseSensitive(NEM, "data");
+
+	cJSON *currentNEMtime = cJSON_GetObjectItemCaseSensitive(data, "currentNEMtime");
+	cJSON *postcode = cJSON_GetObjectItemCaseSensitive(data, "postcode");
+	cJSON *networkProvider = cJSON_GetObjectItemCaseSensitive(data, "networkProvider");
+
+	printf("Current Time: %s\r\n",currentNEMtime->valuestring);
+	printf("Postcode: %s\r\n",postcode->valuestring);
+	printf("Network Provider: %s\r\n",networkProvider->valuestring);
+
+	cJSON *staticPrices = cJSON_GetObjectItemCaseSensitive(data, "staticPrices");
+	//cJSON_ArrayForEach(parameter, staticPrices)
+	//{
+	//	cJSON *totaldailyprice = cJSON_GetObjectItemCaseSensitive(parameter, "totalDailyPrice");
+	//	printf("Total Daily Price = %.02f c/kWh\r\n", strtod(totaldailyprice->valuestring, NULL));
+	//}
+
+	cJSON *variablePrices = cJSON_GetObjectItemCaseSensitive(data, "variablePricesAndRenewables");
+	cJSON_ArrayForEach(parameter, variablePrices)
+	{
+		cJSON *period = cJSON_GetObjectItemCaseSensitive(parameter, "period");
+		printf("Period: %s ",period->valuestring);
+
+		cJSON *price = cJSON_GetObjectItemCaseSensitive(parameter, "wholesaleKWHPrice");
+		//printf("Price: %s \r\n",price->valuestring);
+
+		price_kwh = strtod(price->valuestring, NULL);
+		printf("Price = %.02f c/kWh\r\n",price_kwh);
+	}
+
+	cJSON_Delete(NEM);
 }
